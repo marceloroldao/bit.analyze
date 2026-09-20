1bd8621ae1ba295b64fa62a27792d015bdcabd9a
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-09-20 20:48:44 -0300

    feat: add guarded baseline update contract

--- include/bit_analyze/structural_baseline.hpp
+++ include/bit_analyze/structural_baseline.hpp
@@ -7,11 +7,13 @@ struct StructuralBaselineState {
  double mean_extent{}, mean_intensity{};
  double m2_extent{}, m2_intensity{};
 };
+struct StructuralBaselineUpdate { StructuralChangeVector deviation{}; bool accepted{}; };
 class StructuralBaseline {
 public:
  void observe(StructuralChangeVector v) noexcept;
  const StructuralBaselineState& state() const noexcept { return state_; }
  StructuralChangeVector deviation(StructuralChangeVector v) const noexcept;
+ StructuralBaselineUpdate observe_if_consistent(StructuralChangeVector v, double max_deviation) noexcept;
 private:
  StructuralBaselineState state_{};
 };
